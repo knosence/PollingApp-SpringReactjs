cce29672b185d66dcf035aa775a1a56f827f0bbc
Kno Sence <knosence@linux-xibd.hsd1.ga.comcast.net>
2018-09-28 11:47:21 -0400

    Debugging

--- src/main/java/com/knosence/springreact/pollingappspringreact/Models/User.java
+++ src/main/java/com/knosence/springreact/pollingappspringreact/Models/User.java
@@ -17,11 +17,15 @@ import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.knosence.springreact.pollingappspringreact.Models.audit.DataAudit;
+
 @Entity
 @Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = { "username" }),
         @UniqueConstraint(columnNames = { "email" }) })
 
-public class User extends DateAudit {
+public class User extends DataAudit {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
