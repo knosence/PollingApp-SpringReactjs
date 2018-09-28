c35d60d521bc665903e3a344fd3d0347f0c0ec1c
Kno Sence <knosence@linux-xibd.hsd1.ga.comcast.net>
2018-09-28 00:34:24 -0400

    added User.java Model and set up folder structure

--- src/main/java/com/knosence/springreact/pollingappspringreact/PollingappSpringreactApplication.java
+++ src/main/java/com/knosence/springreact/pollingappspringreact/PollingappSpringreactApplication.java
@@ -1,10 +1,26 @@
 package com.knosence.springreact.pollingappspringreact;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+    PollingappSpringreactApplication.class,
+    Jsr310Converters.class
+})
+
 public class PollingappSpringreactApplication {
+
+    @PostConstruct
+    void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PollingappSpringreactApplication.class, args);
